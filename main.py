b3f202cdc3b1f54151d3935541efdb507bee98af
Yevhen Pelikh <pelikh123.evg@gmail.com>
2023-05-16 10:42:27 +0200

    changed possition of rect and new colors set

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import pygame
 import sys
-from random import randint
-
-clock = pygame.time.Clock()
 
 pygame.init()
 
-screen = pygame.display.set_mode((600, 400))
-
 pygame.display.set_caption('Space Shooter')
 
+screen_width = 600
+screen_height = 400
+screen = pygame.display.set_mode((screen_width, screen_height))
+fill_color = (32, 52, 71)
+
+rect_width = 100
+rect_hegiht = 200
+rect_x = (screen_width / 2) - (rect_width / 2)
+rect_y = (screen_height / 2) - (rect_hegiht / 2)
+rect_color = pygame.Color('lightyellow')
 
 while True:
-    for event in  pygame.event.get():
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
-    screen.fill((randint(0, 255), randint(0, 255), randint(0, 255)))
+    screen.fill((fill_color))
+    pygame.draw.rect(screen, rect_color, (rect_x, rect_y, rect_width, rect_hegiht))
     pygame.display.update()
-
-    clock.tick(5)
